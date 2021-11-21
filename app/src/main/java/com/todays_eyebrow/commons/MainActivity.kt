02858382f45e247072cb7df7a49970326095463e
ngh_0 <ghks0924>
2021-11-21 21:27:42 +0900

    repositories edit

--- app/src/main/java/com/todays_eyebrow/commons/MainActivity.kt
+++ app/src/main/java/com/todays_eyebrow/commons/MainActivity.kt
@@ -2,14 +2,12 @@ package com.todays_eyebrow.commons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.todays_eyebrow.commonlib.TestLib
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        TestLib.showToast(this, "첫 발걸음")
 
     }
 }
